dc427d43b4060b30ce183241400779667ffe2c7a
dellpc <dellpc@DELL>
2017-07-10 09:24:28 +0800

    1

--- Geomystery/Option.xaml.cs
+++ Geomystery/Option.xaml.cs
@@ -20,9 +20,9 @@ namespace Geomystery
     /// <summary>
     /// 可用于自身或导航至 Frame 内部的空白页。
     /// </summary>
-    public sealed partial class BlankPage1 : Page
+    public sealed partial class Option : Page
     {
-        public BlankPage1()
+        public Option()
         {
             this.InitializeComponent();
         }
