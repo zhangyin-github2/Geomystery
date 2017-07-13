9674756c6baa9d7912263b34e4da2b51cadf344f
imczz <790608522@qq.com>
2017-07-13 09:01:06 +0800

    修复上一次发布不完善的地方

--- Geomystery/Controllers/Geometry/controllers.cs
+++ Geomystery/Controllers/Geometry/controllers.cs
@@ -11,9 +11,12 @@ namespace Geomystery.Controllers.Geometry
     public class controllers
     {
         //逻辑坐标系
-        public Coordinate coord { get; set}
+        public Coordinate coord { get; set; }
 
         //用来显示的坐标系
         public OutputCoordinate outCoord { get; set; }
+
+
+        //public int MyProperty { get; set; }
     }
 }
