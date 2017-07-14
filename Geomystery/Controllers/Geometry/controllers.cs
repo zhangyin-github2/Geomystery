de51bc87a8716ba4a73d0dbadda002f3706d0259
imczz <790608522@qq.com>
2017-07-14 09:22:09 +0800

    模型控制器的注释

--- Geomystery/Controllers/Geometry/controllers.cs
+++ Geomystery/Controllers/Geometry/controllers.cs
@@ -11,22 +11,36 @@ using Windows.UI.Xaml.Input;
 
 namespace Geomystery.Controllers.Geometry
 {
-    //
+    /// <summary>
+    /// 模型控制器，提供点击pointer_pressed与释放pointer_released接口
+    /// </summary>
     public class controllers
     {
-        //逻辑坐标系
+        /// <summary>
+        /// 受控制的逻辑坐标系
+        /// </summary>
         public Coordinate coord { get; set; }
 
-        //用来显示的坐标系
+        /// <summary>
+        /// 用来显示的坐标系，将会在canvas中显示
+        /// </summary>
         public OutputCoordinate outCoord { get; set; }
 
-        //按下
+        /// <summary>
+        /// 按下
+        /// </summary>
+        /// <param name="sender">CanvasAnimatedControl的一个实例canvas1</param>
+        /// <param name="e">点击时的信息，包括坐标、滑轮和按键等</param>
         public void PointerPressed(object sender, PointerRoutedEventArgs e)
         {
             Point pxy = e.GetCurrentPoint((CanvasAnimatedControl)sender).Position;
         }
 
-        //弹起
+        /// <summary>
+        /// 弹起
+        /// </summary>
+        /// <param name="sender">CanvasAnimatedControl的一个实例canvas1</param>
+        /// <param name="e">点击时的信息，包括坐标、滑轮和按键等</param>
         public void canvas1_PointerReleased(object sender, PointerRoutedEventArgs e)
         {
             Point pxy = e.GetCurrentPoint((CanvasAnimatedControl)sender).Position;
