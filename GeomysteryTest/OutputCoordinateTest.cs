01f20a40bb38b275e5f1c76b848b3c2cefb127dc
imczz <790608522@qq.com>
2017-07-23 17:02:53 +0800

    第一个单元测试

---
+++ GeomysteryTest/OutputCoordinateTest.cs
@@ -0,0 +1,26 @@
+﻿using Geomystery.Views.Geometry;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeomysteryTest
+{
+    [TestClass]
+    public class OutputCoordinateTest
+    {
+        [TestMethod]
+        public void TestDistanceOfPointAndLine()
+        {
+            Vector2 lpo = new Vector2() { X = 0, Y = 10 };
+            Vector2 lv = new Vector2() { X = 1, Y = 1 } ;
+            Vector2 outerPoint = new Vector2() { X = 10, Y = 10 };
+            Vector2 result = new Vector2();
+            float distance = OutputCoordinate.DistanceOfPointAndLine(lpo, lv, outerPoint,ref result);
+            Assert.AreEqual(distance,5 * Math.Sqrt(2),0.0001);
+        }
+    }
+}
