26d96dd5888b50eafe6e61976c37aaa287261673
imczz <790608522@qq.com>
2017-07-25 13:23:07 +0800

    新增矩阵.clsss

---
+++ Geomystery/Models/FMatrix/FMatrix.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geomystery.Models.FMatrix
+{
+    /// <summary>
+    /// 自定义的矩阵，自由的矩阵
+    /// </summary>
+    public class FMatrix
+    {
+        /// <summary>
+        /// 行
+        /// </summary>
+        public int row { get; set; }
+
+        /// <summary>
+        /// 列
+        /// </summary>
+        public int column { get; set; }
+
+
+    }
+}
