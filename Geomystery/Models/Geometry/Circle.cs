138aa1ecb157b2ed895ecd4050b8d68efdd7ea7e
imczz <790608522@qq.com>
2017-07-27 02:57:39 +0800

    IPointSet的交点实现完成，单元测试

--- Geomystery/Models/Geometry/Circle.cs
+++ Geomystery/Models/Geometry/Circle.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -63,6 +64,40 @@ namespace Geomystery.Models.Geometry
                 if(distanceOfTwoCircle >= Math.Abs(radius1 - radius2) && distanceOfTwoCircle <= radius1 + radius2)          //存在交点(内切、外切、相交)
                 {
                     //TODO:两个圆相交
+                    int method = 1;
+                    if(method==1)                           //平面几何计算解析几何
+                    {
+                        //余弦定理 cosA = (b^2 + c^2 - a^2)/(2bc)
+                        double cosA = (Math.Pow(radius1, 2.0) + Math.Pow(distanceOfTwoCircle, 2.0) - Math.Pow(radius2, 2.0)) / 2 / radius1 / distanceOfTwoCircle;
+                        double sinA = Math.Sqrt(1 - Math.Pow(cosA, 2.0));
+                        double lengthOfCenterAndBisector = radius1 * cosA;                  //圆心到垂足长度
+                        double halfChord = radius1 * sinA;                                  //弦长的一半
+                        Vector2 vectorR1ToR2 = new Vector2() { X = c2.center.X - this.center.X, Y = c2.center.Y - this.center.Y };
+                        Vector2 ve = Vector2.Normalize(vectorR1ToR2);                   //圆心法向量
+                        Vector2 vb = ve * (float)lengthOfCenterAndBisector;         //偏移
+                        Vector2 vep = new Vector2() { X = ve.Y, Y = -ve.X };            //弦直线的法向量
+                        Vector2 vepb1 = vep * (float)halfChord;                     //一个点
+                        Vector2 vepb2 = -vep * (float)halfChord;                    //另一个点
+                        Vector2 bbbb = new Vector2() { X = this.center.X + vb.X, Y = this.center.Y + vb.Y };
+                        pcl.Add(new Point2() { X = bbbb.X + vepb1.X, Y = bbbb.Y + vepb1.Y });
+                        pcl.Add(new Point2() { X = bbbb.X + vepb2.X, Y = bbbb.Y + vepb2.Y });
+                    }
+                    else if(method == 2)                    //解析几何方法
+                    {
+                        //TODO:联立两圆标准方程，得到公共弦方程，联立公共弦方程与其中一个圆，得到交点
+                    }
+                    else if(method == 3)                    //另一种解析几何
+                    {
+                        //TODO:一个圆用标准方程(x - x0)^2 + (y - y0)^2 = r^2
+                        //另一个圆用参数方程x = r * cos θ; y = r * sin θ;
+                        //把参数方程代入标准方程，整理得
+                        //a * cosθ + b * sinθ = c
+                        //其中
+                        //a = 2 * r1 * (x1 - x2)
+                        //b = 2 * r1 * (y1 - y2)
+                        //c = r2 ^ 2 - r1 ^ 2 - (x1 - x2) ^ 2 - (y1 - y2) ^ 2
+                        //通过一些方法解出合理的θ，代入参数方程，即可得到交点
+                    }
                 }
 
             }
