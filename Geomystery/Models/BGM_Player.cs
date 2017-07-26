62a267a81e89eb06020215816c752bb15a6000b1
郑昊 <1056526134@qq.com>
2017-07-26 21:17:04 +0800

    还原25日beta备份

--- Geomystery/Models/BGM_Player.cs
+++ Geomystery/Models/BGM_Player.cs
@@ -3,20 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Windows.UI.Xaml.Controls;
 
 namespace Geomystery.Models
 {
     class BGM_Player
     {
-        public static MediaElement MusicPlayer;
-        public static MediaElement getInstance()
-        {
-            if (MusicPlayer == null)
-            {
-                MusicPlayer = new MediaElement();
-            }
-            return MusicPlayer;
-        }
     }
 }
