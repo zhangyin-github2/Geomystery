804db10fce00fb0a6671d74141b95cb36a86d799
59457 <59457@DESKTOP-U7HN6IG>
2017-07-30 07:23:12 +0800

    chengjiu

--- Geomystery/Award/Achievement.cs
+++ Geomystery/Award/Achievement.cs
@@ -19,47 +19,27 @@ namespace Geomystery.Award
         public int aim { get; set; }
         public int have_done { get; set; }
         public int reward_num { get; set; }
-        public double fontsize1 { get; set; }
-        public double fontsize2 { get; set; }
-        public double fontsize3 { get; set; }
 
-        public Achievements()
+        public Achievements(double ff=44)
         {
             aim = 1;
             have_done = 0;
+            col = new SolidColorBrush(Color.FromArgb(200, 255, 0, 0));
             islock = "未达成";
         }
         public static ObservableCollection<Achievements> GetAch(double ff = 44)
         {
             var ach = new ObservableCollection<Achievements>();
-
-            ach.Add(new Achievements { name = "小试牛刀", picture = "Picture/小试牛刀.png", aim = 1, reward_num = 200, islock = "未达成", col = new SolidColorBrush(Color.FromArgb(200, 255, 0, 0)), fontsize1 = 2 * ff, fontsize2 = ff, fontsize3 = 1.2 * ff });
-            ach.Add(new Achievements { name = "初出茅庐", picture = "Picture/旗开得胜.png", aim=1,reward_num=200, islock = "未达成", col = new SolidColorBrush(Color.FromArgb(200, 255, 0, 0)), fontsize1 = 2 * ff, fontsize2 = ff, fontsize3 = 1.2 * ff });
-            //ach.Add(new Achievements { name = "细心的人", picture = "Picture/细心的人.png", aim = 1, reward_num = 300, islock = "未达成", col = new SolidColorBrush(Color.FromArgb(200, 255, 0, 0)), fontsize1 = 2 * ff, fontsize2 = ff, fontsize3 = 1.2 * ff });
-            //ach.Add(new Achievements { name = "好事多磨", picture = "Picture/好事多磨.png", aim = 1, reward_num = 200, islock = "未达成", col = new SolidColorBrush(Color.FromArgb(200, 255, 0, 0)), fontsize1 = 2 * ff, fontsize2 = ff, fontsize3 = 1.2 * ff });
-            //ach.Add(new Achievements { name = "圆圆不断", picture = "Picture/圆圆不断.png", aim = 1, reward_num = 400, islock = "未达成", col = new SolidColorBrush(Color.FromArgb(200, 255, 0, 0)), fontsize1 = 2 * ff, fontsize2 = ff, fontsize3 = 1.2 * ff });
-            //ach.Add(new Achievements { name = "穿针引线", picture = "Picture/穿针引线.png", aim = 1, reward_num = 400, islock = "未达成", col = new SolidColorBrush(Color.FromArgb(200, 255, 0, 0)), fontsize1 = 2 * ff, fontsize2 = ff, fontsize3 = 1.2 * ff });
-            ach.Add(new Achievements { name = "再接再厉", picture = "Picture/土豪.png", aim = 1, reward_num = 500, islock = "未达成", col = new SolidColorBrush(Color.FromArgb(200, 255, 0, 0)), fontsize1 = 2 * ff, fontsize2 = ff, fontsize3 = 1.2 * ff });
-            ach.Add(new Achievements { name = "解题狂人", picture = "Picture/ 解题狂人.png", aim = 1, reward_num = 1000, islock = "未达成", col = new SolidColorBrush(Color.FromArgb(200, 255, 0, 0)), fontsize1 = 2 * ff, fontsize2 = ff, fontsize3 = 1.2 * ff });
-            ach.Add(new Achievements { name = "数学家", picture = "Picture/数学家.png", aim = 1, reward_num = 1000, islock = "未达成", col = new SolidColorBrush(Color.FromArgb(200, 255, 0, 0)), fontsize1 = 2 * ff, fontsize2 = ff, fontsize3 = 1.2 * ff });
-            ach.Add(new Achievements { name = "几何大师", picture = "Picture/几何大师.png", aim = 1, reward_num = 3000, islock = "未达成", col = new SolidColorBrush(Color.FromArgb(200, 255, 0, 0)), fontsize1 = 2 * ff, fontsize2 = ff, fontsize3 = 1.2 * ff });
-
-            string[] Discirbe = new string[] {
-                "完成了你的第一关，开启了你的解题之旅。",
-                "通关第一章",
-                "通关第二章",
-                "解锁全部关卡。",
-                "通关所有章节",
-                "完美通过所有关卡。",
-            };
-
-            for(int i=0;i< ach.Count;i++)
+   
+            for(int i=0;i< 6;i++)
             {
+                ach.Add(new Achievements());
                 ach[i].name = AppResources.GetString("AC" + (i + 1).ToString() + "N");
                 ach[i].discribe = AppResources.GetString("AC" +(i+1).ToString()+"D");
                 string locked = APPDATA.app_data.ACHIEVEMENT[i] == '0' ? "Lock" : "Unlock";
                 if(APPDATA.app_data.ACHIEVEMENT[i] == '1') ach[i].col = new SolidColorBrush(Color.FromArgb(200, 0, 220, 0));
                 ach[i].islock = AppResources.GetString(locked);
+                ach[i].picture = "ms-appx:///Pictures/Achievement/" + (i + 1).ToString() + ".png";
             }
             return ach;
         }
--- Geomystery/Pages/Achievement.xaml.cs
+++ Geomystery/Pages/Achievement.xaml.cs
@@ -27,9 +27,13 @@ namespace Geomystery
         private ViewModel.ViewModel View { set; get; } = new ViewModel.ViewModel();
         
         public  ObservableCollection<Achievements> ach;
+        public ObservableCollection<TextBlock> nameT = new ObservableCollection<TextBlock>(), disT = new ObservableCollection<TextBlock>(), lockT = new ObservableCollection<TextBlock>();
+        public ObservableCollection<Grid> grids = new ObservableCollection<Grid>();
+        public ObservableCollection<Image> ims = new ObservableCollection<Image>();
         public Achievement()
         {
             this.InitializeComponent();
+            changefont();
             View = new ViewModel.ViewModel();
             if (!APPDATA.app_data.Views.Contains(View))
             {
@@ -37,6 +41,67 @@ namespace Geomystery
             }
             ach = Achievements.GetAch(10);
             APPDATA.app_data.ACHIEVEMENTS = ach;
+        }
+        private void nameTloaded(object sender, RoutedEventArgs e)
+        {
+            var k = sender as TextBlock;
+            nameT.Add(k);
+        }
+        private void disTloaded(object sender, RoutedEventArgs e)
+        {
+            var k = sender as TextBlock;
+            disT.Add(k);
+
+        }
+        private void lockTloaded(object sender, RoutedEventArgs e)
+        {
+            var k = sender as TextBlock;
+            lockT.Add(k);
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            var k = sender as Grid;
+            grids.Add(k);
+        }
+
+        private void Image_Loaded(object sender, RoutedEventArgs e)
+        {
+            var k = sender as Image;
+            ims.Add(k);
+        }
+
+        private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            changefont();
+        }
+
+        void changefont()
+        {
+            double w, h, k;
+            h = Window.Current.Bounds.Height;
+            w = Window.Current.Bounds.Width;
+            k = Math.Min(w / 1920.0, h / 1080.0);
+            foreach (var x in nameT)
+            {
+                x.FontSize = 28 * k;
+            }
+            foreach (var x in disT)
+            {
+                x.FontSize = 24 * k;
+            }
+            foreach (var x in lockT)
+            {
+                x.FontSize = 18 * k;
+            }
+            foreach(var x in grids)
+            {
+                x.Width = 750 * w / 1920.0;          
+            }
+            foreach(var x in ims)
+            {
+                x.Height = x.Width = 80 * k;
+            }
         }
     }
 }
